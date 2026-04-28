8e9fc464219477ed2906c36b536daf23c06d8665
Mirko Lana <lana.mirko@icloud.com>
2026-04-28 08:50:45 +0200

    refactor: name changed from `temp` to `temperature`

--- Core/Src/bms/temperature-api.c
+++ Core/Src/bms/temperature-api.c
@@ -1,13 +1,13 @@
 /*!
- * \file            temp-api.c
+ * \file            temperature-api.c
  * \date            2026-03-24
  * \authors         Mirko Lana [mirko.lana@eagletrt.it]
  *
  * \brief           Temperature module APIs.
  */
 
-#include "temp.h"
-#include "temp-api.h"
+#include "temperature.h"
+#include "temperature-api.h"
 
 #include <stddef.h>
 #include <stdbool.h>
@@ -20,37 +20,37 @@
 
 #ifdef CONFIG_TEMPERATURE_MODULE_ENABLE
 
-EAGLETRT_STATIC struct TempHandler htemp; /*!< Private temperature handler instance. */
+EAGLETRT_STATIC struct TemperatureHandler htemp; /*!< Private temperature handler instance. */
 
-void temp_api_init(void) {
+void temperature_api_init(void) {
     memset(&htemp, 0U, sizeof(htemp));
 }
 
-enum TempReturnCode temp_api_update_temperature(size_t idx, celsius temperature) {
+enum TemperatureReturnCode temperature_api_update_temperature(size_t idx, celsius temperature) {
     if (idx >= DEFS_CELLS_COUNT) {
-        return TEMP_RC_OUT_OF_BOUNDS;
+        return TEMPERATURE_RC_OUT_OF_BOUNDS;
     }
 
     htemp.temperatures[idx] = temperature;
-    return TEMP_RC_OK;
+    return TEMPERATURE_RC_OK;
 }
 
-enum TempReturnCode temp_api_update_temperatures(size_t idx, const celsius *temperatures, size_t size) {
+enum TemperatureReturnCode temperature_api_update_temperatures(size_t idx, const celsius *temperatures, size_t size) {
     if (temperatures == NULL) {
-        return TEMP_RC_NULL_POINTER;
+        return TEMPERATURE_RC_NULL_POINTER;
     }
 
     if ((idx + size) > DEFS_CELLS_COUNT) {
-        return TEMP_RC_OUT_OF_BOUNDS;
+        return TEMPERATURE_RC_OUT_OF_BOUNDS;
     }
 
     for (size_t i = 0U; i < size; ++i) {
         htemp.temperatures[idx + i] = temperatures[i];
     }
-    return TEMP_RC_OK;
+    return TEMPERATURE_RC_OK;
 }
 
-celsius temp_api_get_min(void) {
+celsius temperature_api_get_min(void) {
     celsius min = htemp.temperatures[0];
     for (size_t i = 0; i < DEFS_CELLS_COUNT; ++i) {
         min = EAGLETRT_API_MIN(min, htemp.temperatures[i]);
@@ -59,7 +59,7 @@ celsius temp_api_get_min(void) {
     return min;
 }
 
-celsius temp_api_get_max(void) {
+celsius temperature_api_get_max(void) {
     celsius max = htemp.temperatures[0];
     for (size_t i = 0; i < DEFS_CELLS_COUNT; ++i) {
         max = EAGLETRT_API_MAX(max, htemp.temperatures[i]);
@@ -68,7 +68,7 @@ celsius temp_api_get_max(void) {
     return max;
 }
 
-celsius temp_api_get_average(void) {
+celsius temperature_api_get_average(void) {
     celsius average = 0.f;
     for (size_t i = 0; i < DEFS_CELLS_COUNT; ++i) {
         average += htemp.temperatures[i];
@@ -77,17 +77,17 @@ celsius temp_api_get_average(void) {
     return average / DEFS_CELLS_COUNT;
 }
 
-enum TempReturnCode temp_api_dump_temperatures(celsius *out, size_t start, size_t size) {
+enum TemperatureReturnCode temperature_api_dump_temperatures(celsius *out, size_t start, size_t size) {
     if (out == NULL) {
-        return TEMP_RC_NULL_POINTER;
+        return TEMPERATURE_RC_NULL_POINTER;
     }
 
     if ((start + size) > DEFS_CELLS_COUNT) {
-        return TEMP_RC_OUT_OF_BOUNDS;
+        return TEMPERATURE_RC_OUT_OF_BOUNDS;
     }
 
     memcpy(out, htemp.temperatures + start, size * sizeof(*out));
-    return TEMP_RC_OK;
+    return TEMPERATURE_RC_OK;
 }
 
-#endif /*! CONFIG_TEMPERATURE_MODULE */
+#endif /*! CONFIG_TEMPERATURE_MODULE_ENABLE */
---
+++ test/test-temperature-api.c
@@ -0,0 +1,232 @@
+/*!
+ * \file            test-temperature-api.h
+ * \date            2026-03-31
+ * \authors         Mirko Lana [mirko.lana@eagletrt.it]
+ *
+ * \brief           Test functions for the temperature module.
+ */
+
+#include "unity.h"
+
+#include <string.h>
+
+#include "temperature.h"
+#include "temperature-api.h"
+#include "types.h"
+#include "defs.h"
+
+extern struct TemperatureHandler htemp;
+
+void setUp(void) {
+    temperature_api_init();
+}
+
+void tearDown() {
+}
+
+/*!
+ * \defgroup		temperature_api_init Test for temperature_api_init function.
+ * \{
+ */
+
+void check_temperature_api_init(void) {
+    celsius temperatures[DEFS_CELLS_COUNT] = { 0.f };
+    memset(htemp.temperatures, 0xFF, DEFS_CELLS_COUNT * sizeof(*htemp.temperatures));
+    temperature_api_init();
+    TEST_ASSERT_EQUAL_MEMORY(temperatures, htemp.temperatures, DEFS_CELLS_COUNT);
+}
+
+/*! \} */
+
+/*!
+ * \defgroup        temperature_api_update_temperature Test for temperature_api_update_temperature function.
+ * \{
+ */
+
+void check_temperature_api_update_temperature_success(void) {
+    TEST_ASSERT_EQUAL_INT_MESSAGE(TEMPERATURE_RC_OK, temperature_api_update_temperature(0U, 7.f), "temperature_api_update_temperature failed!");
+    TEST_ASSERT_EQUAL_FLOAT_MESSAGE(7.f, htemp.temperatures[0U], "Stored temperature is different!");
+}
+
+void check_temperature_api_update_temperature_index_out_of_bounds(void) {
+    TEST_ASSERT_EQUAL_INT(TEMPERATURE_RC_OUT_OF_BOUNDS, temperature_api_update_temperature(DEFS_CELLS_COUNT, 7.f));
+}
+
+/*! \} */
+
+/*!
+ * \defgroup        temperature_api_update_temperatures Test for temperature_api_update_temperatures function.
+ * \{
+ */
+
+void check_temperature_api_update_temperatures_success(void) {
+    const celsius temperatures[DEFS_CELLS_COUNT] = { 1.f, 2.f, 3.f, 4.f, 5.f, 6.f };
+    TEST_ASSERT_EQUAL_INT_MESSAGE(TEMPERATURE_RC_OK, temperature_api_update_temperatures(0U, temperatures, DEFS_CELLS_COUNT), "temperature_api_update_temperatures failed!");
+    TEST_ASSERT_EQUAL_FLOAT_ARRAY_MESSAGE(temperatures, htemp.temperatures, DEFS_CELLS_COUNT, "Stored temperatures are different!");
+}
+
+void check_temperature_api_update_temperatures_null_temperatures(void) {
+    TEST_ASSERT_EQUAL_INT(TEMPERATURE_RC_NULL_POINTER, temperature_api_update_temperatures(0U, NULL, 0U));
+}
+
+void check_temperature_api_update_temperatures_index_out_of_bounds(void) {
+    const celsius temperatures[DEFS_CELLS_COUNT] = { 1.f, 2.f, 3.f, 4.f, 5.f, 6.f };
+    TEST_ASSERT_EQUAL_INT_MESSAGE(TEMPERATURE_RC_OUT_OF_BOUNDS, temperature_api_update_temperatures(DEFS_CELLS_COUNT, temperatures, DEFS_CELLS_COUNT), "temperature_api_update_temperatures failed!");
+}
+
+void check_temperature_api_update_temperatures_size_too_big(void) {
+#define COUNT (9U)
+    const celsius temperatures[COUNT] = { 1.f, 2.f, 3.f, 4.f, 5.f, 6.f, 7.f, 8.f, 9.f };
+    TEST_ASSERT_EQUAL_INT_MESSAGE(TEMPERATURE_RC_OUT_OF_BOUNDS, temperature_api_update_temperatures(0U, temperatures, COUNT), "temperature_api_update_temperatures failed!");
+#undef COUNT
+}
+
+/*! \} */
+
+/*!
+ * \defgroup        temperature_api_get_min Test for temperature_api_get_min function.
+ * \{
+ */
+
+void check_temperature_api_get_min(void) {
+    const celsius temperatures[DEFS_CELLS_COUNT] = { 1.f, 2.f, 3.f, 4.f, 5.f, 6.f };
+    TEST_ASSERT_EQUAL_INT_MESSAGE(TEMPERATURE_RC_OK, temperature_api_update_temperatures(0U, temperatures, DEFS_CELLS_COUNT), "temperature_api_update_temperatures failed!");
+    TEST_ASSERT_EQUAL_FLOAT_MESSAGE(temperatures[0U], temperature_api_get_min(), "temperatures are different!");
+}
+
+/*! \} */
+
+/*!
+ * \defgroup        temperature_api_get_max Test for temperature_api_get_max function.
+ * \{
+ */
+
+void check_temperature_api_get_max(void) {
+    const celsius temperatures[DEFS_CELLS_COUNT] = { 1.f, 2.f, 3.f, 4.f, 5.f, 6.f };
+    TEST_ASSERT_EQUAL_INT_MESSAGE(TEMPERATURE_RC_OK, temperature_api_update_temperatures(0U, temperatures, DEFS_CELLS_COUNT), "temperature_api_update_temperatures failed!");
+    TEST_ASSERT_EQUAL_FLOAT_MESSAGE(temperatures[5U], temperature_api_get_max(), "temperatures are different!");
+}
+
+/*! \} */
+
+/*!
+ * \defgroup        temperature_api_get_average Test for temperature_api_get_average function.
+ * \{
+ */
+
+void check_temperature_api_get_average(void) {
+    const celsius temperatures[DEFS_CELLS_COUNT] = { 1.f, 2.f, 3.f, 4.f, 5.f, 6.f };
+    TEST_ASSERT_EQUAL_INT_MESSAGE(TEMPERATURE_RC_OK, temperature_api_update_temperatures(0U, temperatures, DEFS_CELLS_COUNT), "temperature_api_update_temperatures failed!");
+    TEST_ASSERT_EQUAL_FLOAT_MESSAGE(3.5f, temperature_api_get_average(), "temperatures are different!");
+}
+
+/*! \} */
+
+/*!
+ * \defgroup        temperature_api_dump_temperatures Test for temperature_api_dump_temperatures function.
+ * \{
+ */
+
+void check_temperature_api_dump_temperatures_success(void) {
+#define COUNT (3U)
+    celsius dump[COUNT] = { 0 };
+    TEST_ASSERT_EQUAL_INT_MESSAGE(TEMPERATURE_RC_OK, temperature_api_dump_temperatures(dump, 2U, COUNT), "temperature_api_dump_temperatures failed!");
+    TEST_ASSERT_EQUAL_FLOAT_ARRAY_MESSAGE(htemp.temperatures + 2U, dump, COUNT, "temperatures are different!");
+#undef COUNT
+}
+
+void check_temperature_api_dump_temperatures_null_out(void) {
+    TEST_ASSERT_EQUAL_INT(TEMPERATURE_RC_NULL_POINTER, temperature_api_dump_temperatures(NULL, 0U, 0U));
+}
+
+void check_temperature_api_dump_temperatures_start_out_of_bounds(void) {
+#define COUNT (3U)
+    celsius dump[3U] = { 0 };
+    TEST_ASSERT_EQUAL_INT(TEMPERATURE_RC_OUT_OF_BOUNDS, temperature_api_dump_temperatures(dump, DEFS_CELLS_COUNT, COUNT));
+#undef COUNT
+}
+
+void check_temperature_api_dump_temperatures_size_too_big(void) {
+#define COUNT (8U)
+    celsius dump[COUNT] = { 0 };
+    TEST_ASSERT_EQUAL_INT(TEMPERATURE_RC_OUT_OF_BOUNDS, temperature_api_dump_temperatures(dump, 0U, COUNT));
+#undef COUNT
+}
+
+/*! \} */
+
+int main(void) {
+    UNITY_BEGIN();
+
+    /*!
+     * \defgroup		temperature_api_init Test for temperature_api_init function.
+     * \{
+     */
+
+    RUN_TEST(check_temperature_api_init);
+
+    /*! \} */
+
+    /*!
+     * \defgroup        temperature_api_update_temperature Test for temperature_api_update_temperature function.
+     * \{
+     */
+
+    RUN_TEST(check_temperature_api_update_temperature_success);
+    RUN_TEST(check_temperature_api_update_temperature_index_out_of_bounds);
+
+    /*! \} */
+
+    /*!
+     * \defgroup        temperature_api_update_temperatures Test for temperature_api_update_temperatures function.
+     * \{
+     */
+
+    RUN_TEST(check_temperature_api_update_temperatures_success);
+    RUN_TEST(check_temperature_api_update_temperatures_null_temperatures);
+    RUN_TEST(check_temperature_api_update_temperatures_index_out_of_bounds);
+    RUN_TEST(check_temperature_api_update_temperatures_size_too_big);
+
+    /*! \} */
+
+    /*!
+	 * \defgroup        temperature_api_get_min Test for temperature_api_get_min function.
+	 * \{
+	 */
+
+    RUN_TEST(check_temperature_api_get_min);
+
+    /*! \} */
+
+    /*!
+	 * \defgroup        temperature_api_get_max Test for temperature_api_get_max function.
+	 * \{
+	 */
+
+    RUN_TEST(check_temperature_api_get_max);
+
+    /*! \} */
+
+    /*!
+	 * \defgroup        temperature_api_get_average Test for temperature_api_get_average function.
+	 * \{
+	 */
+
+    RUN_TEST(check_temperature_api_get_average);
+
+    /*! \} */
+
+    /*!
+     * \defgroup        temperature_api_dump_temperatures Test for temperature_api_dump_temperatures function.
+     * \{
+     */
+
+    RUN_TEST(check_temperature_api_dump_temperatures_success);
+    RUN_TEST(check_temperature_api_dump_temperatures_null_out);
+    RUN_TEST(check_temperature_api_dump_temperatures_start_out_of_bounds);
+    RUN_TEST(check_temperature_api_dump_temperatures_size_too_big);
+
+    /*! \} */
+
+    UNITY_END();
+}
