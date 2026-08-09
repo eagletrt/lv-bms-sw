16a12a52a2fb60ed4f2c3acc3d916288e2ced977
killzyy <majorfrost173@gmail.com>
2026-08-09 03:52:25 +0200

    feat: added initial dispatch logic to router

--- Core/Src/bms/can-communication/can-communication-router-api.c
+++ Core/Src/bms/can-communication/can-communication-router-api.c
@@ -8,13 +8,31 @@
  */
 
 #include "can-communication-router-api.h"
+#include "can-primary-api.h"
+
+EAGLETRT_STATIC void prv_dispatch_rx(uint32_t id, union CanPrimaryMessages message) {
+    EAGLETRT_API_UNUSED(message);
+    switch (id) {
+        default:
+            break;
+    }
+}
 
 enum CanCommunicationReturnCode can_communication_router_api_receive_primary(const struct CanCommunicationFrame *frame) {
     if (frame == NULL) {
         return CAN_COMMUNICATION_RC_NULL_POINTER;
     }
 
-    // TODO: add libcan deserialization and dispatch logic here
+    if (!can_primary_api_id_is_valid(frame->id)) {
+        return CAN_COMMUNICATION_RC_INVALID_NETWORK;
+    }
+
+    union CanPrimaryMessages message = { 0 };
+    if (can_primary_api_deserialize_from_id(frame->id, (uint8_t *)frame->data, &message) != 0) {
+        return CAN_COMMUNICATION_RC_ERROR;
+    }
+
+    prv_dispatch_rx(frame->id, message);
 
     return CAN_COMMUNICATION_RC_OK;
 }
